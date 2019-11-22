f009c369bfb9578be45ed05c8f38be59d28aaa49
Sooraj Ravindran <ravindran.sooraj3@gmail.com>
2019-11-22 19:37:23 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -3,9 +3,13 @@
 int linear_search(int arr[], int n, int x) 
 { 
     int i; 
-    for (i = 0; i < n; i++) 
-        if (arr[i] == x) 
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
             return i; 
+        }
+    }
     return -1; 
 } 
   
@@ -15,8 +19,13 @@ int main(void)
     int x = 10; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     int result = linear_search(arr, n, x); 
-    (result == -1) ? printf("Element is not present in array") 
-                   : printf("Element is present at index %d", 
-                            result); 
+    if (result == -1)
+    {
+         printf("Element is not present in array");
+    }
+    else
+    {
+        printf("Element is present at index %d", result);
+    }
     return 0; 
 } 
