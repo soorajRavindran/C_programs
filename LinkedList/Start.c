f02128c41604e8f96805cd849d02ba1ba90b6f05
Sooraj Ravindran <ravindran.sooraj3@gmail.com>
2019-12-11 10:56:29 +0530

    Update Start.c

--- LinkedList/Start.c
+++ LinkedList/Start.c
@@ -9,6 +9,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 #include <stdlib.h>
 #define NUMBER_OF_NODES 5
+#define KEY 8
 
 struct node
 {
@@ -20,6 +21,10 @@ struct node* head = NULL;
 
 void printList();
 struct node* createNodeAtEnd(int data);
+void addNodeAtbeg(int data);
+void addInBetween(int data,int pos);
+void search(int key);
+void deleteNode(int pos);
 
 int main()
 {
@@ -29,6 +34,20 @@ int main()
     {
       head=createNodeAtEnd(i);
     }
+    
+    printList();
+    
+    addNodeAtbeg(5);
+    
+    printList();
+    
+    search(KEY);
+    
+    addInBetween(6, 3);
+    
+    printList();
+    
+    deleteNode(8);
     
     printList();
 
@@ -55,6 +74,49 @@ struct node* createNodeAtEnd(int data)
     return head;
 }
 
+void addNodeAtbeg(int data)
+{
+    struct node* p;
+    struct node* temp = (struct node *)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->link=head;
+    head=temp;
+}
+
+void addInBetween(int data,int pos)
+{
+    int count =0;
+    struct node* i;
+    
+    struct node* p=head;
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    
+    while(count!=(pos-1))
+    {
+        p=p->link;
+        count++;
+    }
+    
+    temp->data=data;
+    i=p->link;
+    p->link=temp;
+    temp->link=i;
+    
+}
+
+void deleteNode(int pos)
+{
+    struct node* p= head;
+    int count=1;
+    
+    while(count!=(pos-1))
+    {
+        p=p->link;
+        count++;
+    }
+    p->link=(p->link)->link;
+}
+
 void printList()
 {
     struct node* p = head;
@@ -62,10 +124,25 @@ void printList()
     {
         printf("List is empty \n");
     }
-    printf("Value at each node :");
+    printf("\n Value at each node :");
     while(p!=NULL)
     {
         printf("%d \t", p->data);
         p=p->link;
     }
+}
+
+void search(int key)
+{
+    struct node* p=head;
+    while(p!=NULL)
+    {
+        if(p->data == key)
+        {
+            printf("\n Search key found");
+            return 1;
+        }
+        p=p->link;
+    }
+    printf("\n Search key not found");
 }
