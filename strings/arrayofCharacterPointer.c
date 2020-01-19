130e69f23f8ada73ba28f5fdd1d51cb1fa61ce6e
Sooraj Ravindran <ravindran.sooraj3@gmail.com>
2020-01-19 08:31:24 +0530

    Create arrayofCharacterPointer.c

---
+++ strings/arrayofCharacterPointer.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char* arr[]={ "white", "black","red","blue","yellow"};
+    
+    printf("%s \n",arr[4]);
+    
+    printf("%c \n", arr[3][2]);
+
+    return 0;
+}
