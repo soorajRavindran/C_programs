6e01372ea843bb0e364ee94d5481b3bcf99959ed
Sooraj Ravindran <ravindran.sooraj3@gmail.com>
2020-04-04 11:10:30 +0530

    Update basics.c

--- C Quest/basics.c
+++ C Quest/basics.c
@@ -33,3 +33,26 @@ main.c:17:0: warning: "A" redefined
 main.c:10:0: note: this is the location of the previous definition                                                            
 10                                                                                                                            
 20
+    
+    
+    
+#include <stdio.h>
+
+int main()
+{
+    char *p = "Sooraj";
+    printf("%s \n",p);
+    
+    printf("%c \n",*(p+1));
+    printf("%c",*(p+3));
+    
+    *(p+2) = 'e';
+    
+    printf("%c",*(p+3));
+}
+
+output
+======================
+Sooraj                                                                                                                        
+o                                                                                                                             
+Segmentation fault (core dumped)
