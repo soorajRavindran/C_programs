8cbe0f97f37633e68f79be339cff93029593d178
Sooraj Ravindran <ravindran.sooraj3@gmail.com>
2020-04-04 10:58:26 +0530

    Update basics.c

--- C Quest/basics.c
+++ C Quest/basics.c
@@ -12,3 +12,24 @@ int main()
 output
 ==============
 10 20 30
+
+    
+#include <stdio.h>
+#define A 10
+
+int main()
+{
+    int a = A;
+    printf("%d \n", a);
+#define A 20
+    a = A;
+    printf("%d \n", a);
+    return 0;
+}
+
+output
+=========================
+main.c:17:0: warning: "A" redefined                                                                                           
+main.c:10:0: note: this is the location of the previous definition                                                            
+10                                                                                                                            
+20
